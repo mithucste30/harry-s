ed82e684240cad7422f25c501bc52b90467d28d6
MD. MAZHARUL ISLAM <mithu.cste.30@gmail.com>
2014-08-27 19:05:46 +0600

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,25 +13,25 @@ class User < ActiveRecord::Base
     REFERRAL_STEPS = [
         {
             'count' => 5,
-            "html" => "Shave<br>Cream",
+            "html" => "2 eBooks<br/>valued at $14",
             "class" => "two",
             "image" =>  ActionController::Base.helpers.asset_path("refer/cream-tooltip@2x.png")
         },
         {
             'count' => 10,
-            "html" => "Truman Handle<br>w/ Blade",
+            "html" => "2 eBooks<br/>valued at $35",
             "class" => "three",
             "image" => ActionController::Base.helpers.asset_path("refer/truman@2x.png")
         },
         {
             'count' => 25,
-            "html" => "Winston<br>Shave Set",
+            "html" => "5 eBooks + Set of<br/>3 Gift Cards valued at $50",
             "class" => "four",
             "image" => ActionController::Base.helpers.asset_path("refer/winston@2x.png")
         },
         {
             'count' => 50,
-            "html" => "One Year<br>Free Blades",
+            "html" => "5 eBooks + Set of 7 Gift Cards + 1 PSYCH-K session with Cara Phillips",
             "class" => "five",
             "image" => ActionController::Base.helpers.asset_path("refer/blade-explain@2x.png")
         }
