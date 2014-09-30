b66ac9b75ebcea1913759e67dd08a929d8e512bd
Md. Mazharul Islam <mithu.cste.30@gmail.com>
2014-09-30 19:03:17 +0600

    corrections

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,25 +15,25 @@ class User < ActiveRecord::Base
             'count' => 5,
             "html" => "2 eBooks<br/>valued at $14",
             "class" => "two",
-            "image" =>  ActionController::Base.helpers.asset_path("refer/cream-tooltip@2x.png")
+            "image" =>  ActionController::Base.helpers.asset_path("refer/product_1.jpg")
         },
         {
             'count' => 10,
             "html" => "5 eBooks<br/>valued at $35",
             "class" => "three",
-            "image" => ActionController::Base.helpers.asset_path("refer/truman@2x.png")
+            "image" => ActionController::Base.helpers.asset_path("refer/product_2.jpg")
         },
         {
             'count' => 25,
             "html" => "5 eBooks + Set of<br/>3 Gift Cards valued at $50",
             "class" => "four",
-            "image" => ActionController::Base.helpers.asset_path("refer/winston@2x.png")
+            "image" => ActionController::Base.helpers.asset_path("refer/product_3.jpg")
         },
         {
             'count' => 50,
             "html" => "5 eBooks + Set of 7 Gift Cards + 1 PSYCH-K session with Cara Phillips",
             "class" => "five",
-            "image" => ActionController::Base.helpers.asset_path("refer/blade-explain@2x.png")
+            "image" => ActionController::Base.helpers.asset_path("refer/product_4.jpg")
         }
     ]
 
